a6fb09b366ff6828835d32d7fd18881111165f01
BTa7BxrHYn <dv@plaksyuk.com>
2026-01-28 20:10:40 +0400

    Move both widgets to view_projects_show_right hook (Dashboard compatibility)

--- lib/redmineup_projects_time_tracking/hooks.rb
+++ lib/redmineup_projects_time_tracking/hooks.rb
@@ -2,10 +2,10 @@
 
 module RedmineupProjectsTimeTracking
   class Hooks < Redmine::Hook::ViewListener
-    # Add metrics box to project overview page (right sidebar)
+    # Add metrics box to project overview page
     render_on :view_projects_show_right, partial: 'projects/ptt_metrics_box'
 
-    # Add history box to project overview page (bottom)
-    render_on :view_projects_show_left, partial: 'projects/ptt_history_box'
+    # Add history box to project overview page (same hook - Dashboard plugin ignores _left)
+    render_on :view_projects_show_right, partial: 'projects/ptt_history_box'
   end
 end
