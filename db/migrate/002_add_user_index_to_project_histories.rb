6e31f16099c15fd00b05a69a61556300bb186d07
BTa7BxrHYn <dv@plaksyuk.com>
2026-01-28 19:02:24 +0400

    Fix migration: check if index exists before creating

--- db/migrate/002_add_user_index_to_project_histories.rb
+++ db/migrate/002_add_user_index_to_project_histories.rb
@@ -2,6 +2,8 @@
 
 class AddUserIndexToProjectHistories < ActiveRecord::Migration[5.2]
   def change
-    add_index :ptt_project_histories, :user_id, name: 'index_ptt_project_histories_on_user_id'
+    unless index_exists?(:ptt_project_histories, :user_id, name: 'index_ptt_project_histories_on_user_id')
+      add_index :ptt_project_histories, :user_id, name: 'index_ptt_project_histories_on_user_id'
+    end
   end
 end
